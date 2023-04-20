f644400841a19cb6291d653a30f8f480cf866809
M-Farag <mina.farag@icloud.com>
2023-04-20 18:46:16 +0200

    ADD New cli crate structopt
    UPD Accepting the legnth from the user

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use rand::seq::SliceRandom;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name="Password generator",about="A strong password generator")]
+struct Arguments {
+    #[structopt(long="length", short="l", default_value="8")]
+    length:usize
+}
 
 fn main() {
     // Todo
-    // - [x] build a logic to generate a password
-    // - [x] testing that everything works
+    // - [x] Adding the structopt crate
+    // - [x] Use the struct opt in the main.rs
+    // - [x] bind and config the arguments expected from the end user
+    // - [x] test that everything works
 
-    let length:usize = 12;
+    let args = Arguments::from_args();
+
+    println!("length is: {}",args.length);
+
     let chars_pool:Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=".chars().collect();
 
-    let password:String = (0..length).map(
+    let password:String = (0..args.length).map(
         |_| {
             * chars_pool.choose(&mut rand::thread_rng()).unwrap()
         }
